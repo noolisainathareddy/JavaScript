169ff2611b3d6673433d609a3426ae5bc0f243d3
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2023-12-20 22:26:12 -0600

    JavaScript basics

---
+++ Basics/Operators.js
@@ -0,0 +1,7 @@
+var isMember = true;
+
+(isMember ? console.log("He is a member") : console.log("He is NOT a member"));
+
+isMember = false;
+
+(isMember ? console.log("He is a member") : console.log("He is NOT a member"));
---
+++ Basics/Variables.js
@@ -0,0 +1,15 @@
+const uid = "abc123";
+
+var fullName= 'Sai natha Reddy Nooli';
+var email = "sns@gmail.com";
+var password= "1934sai@";
+var confrimPassword = "1934sai@";
+var courseCount =0;
+var isLoggedInFromGoogle = false; 
+
+
+console.log(`
+    fullName : ${fullName}
+    Email: ${email}
+    password: ${password}
+`);
