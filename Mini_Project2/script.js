e7593c098915863db1d7bb12f8f2afdf75fa5d54
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2023-12-26 00:01:04 -0600

    project2

---
+++ Mini_Project2/script.js
@@ -0,0 +1,10 @@
+const red = document.querySelector(".color-red");
+const cyan = document.querySelector(".color-cyan");
+const violet = document.querySelector(".color-violet");
+const orange = document.querySelector(".color-orange");
+const pink = document.querySelector(".color-pink");
+
+
+var my_srtyle = window.getComputedStyle(red);
+
+console.log(my_srtyle);
