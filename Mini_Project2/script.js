95a2f24f0a44829841ac0cc8180e82be712bf4a4
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2023-12-26 14:51:03 -0600

    event listners

--- Mini_Project2/script.js
+++ Mini_Project2/script.js
@@ -3,8 +3,30 @@ const cyan = document.querySelector(".color-cyan");
 const violet = document.querySelector(".color-violet");
 const orange = document.querySelector(".color-orange");
 const pink = document.querySelector(".color-pink");
+const subdiv = document.querySelector(".subdiv");
+
+var getBGcolor = (elementName) => {
+    return window.getComputedStyle(elementName).backgroundColor;
+}
+var getPinkColor = getBGcolor(pink);
+var getCyanColor = getBGcolor(cyan);
+var getVioletColor = getBGcolor(violet);
+var getOrangeColor = getBGcolor(orange);
+var getRedColor = getBGcolor(red);
+var count = 1;
+var colorPointer = (pickColor , selectColor) => {
+    pickColor.addEventListener('mouseenter', () => {
+        subdiv.getElementsByTagName('h1')[0].innerText = count;
+        count++;
+        subdiv.style.backgroundColor = selectColor;
+        console.log("Enter into event listner");
+    })
+}
 
 
-var my_srtyle = window.getComputedStyle(red);
+colorPointer(pink,getPinkColor);
+colorPointer(cyan,getCyanColor);
+colorPointer(orange,getCyanColor);
+colorPointer(red,getRedColor);
+colorPointer(violet,getVioletColor); 
 
-console.log(my_srtyle);
